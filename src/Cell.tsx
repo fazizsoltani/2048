2c9be7ee6a66cb10891f90b047b62f4d9ffc5d8f
Your Name <you@example.com>
2022-05-29 19:02:18 +0430

    add check-style

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import './Cell.css'
+import React from "react";
+import "./Cell.css";
 
 interface CellProps {
-  value: number
+  value: number;
 }
 
 interface CellState {}
 
 export default class Cell extends React.Component<CellProps, CellState> {
-  render () {
+  render() {
     return (
-      <div className={`cell cell-${this.props.value}`}>
-        {this.props.value}
-      </div>
-    )
+      <div className={`cell cell-${this.props.value}`}>{this.props.value}</div>
+    );
   }
 }
