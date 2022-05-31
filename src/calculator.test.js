85c7f79b1142d11fd1e02ce7ffa16a4be1d4dcb7
Your Name <you@example.com>
2022-05-31 09:56:57 +0430

    add more unit test

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -8,4 +8,20 @@ describe("Calculator tests", () => {
     // assert
     expect(result).toBe(3);
   });
+
+  test("subtracting 2 from 10 should return 8", () => {
+    // arrange and act
+    var result = mathOperations.diff(10, 2);
+
+    // assert
+    expect(result).toBe(8);
+  });
+
+  test("multiplying 2 and 8 should return 16", () => {
+    // arrange and act
+    var result = mathOperations.product(2, 8);
+
+    // assert
+    expect(result).toBe(16);
+  });
 });
